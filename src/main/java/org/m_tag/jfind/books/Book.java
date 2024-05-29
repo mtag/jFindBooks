6948aa98450ab746281922dfc3e543de8e79b6fe
Masanao TAGUCHI <mtag@m-tag.org>
2024-05-30 08:48:31 +0900

    add constructor from file(not tested)

--- src/main/java/org/m_tag/jfind/books/Book.java
+++ src/main/java/org/m_tag/jfind/books/Book.java
@@ -1,17 +1,20 @@
 package org.m_tag.jfind.books;
 
+import java.nio.file.Path;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Result record of found books.
  */
 public class Book {
+  private static final Pattern pattern =
+      Pattern.compile("^[(]([^)]+)[)] *\\[([^\\]]+)\\] *(.*) *([v第]\\d.*)[.](rar|zip|7z|lzh)$",
+          Pattern.CASE_INSENSITIVE);
   private String author;
-  private String title;
 
-  public String getAuthor() {
-    return author;
-  }
+  private String title;
 
   public Book() {
     super();
@@ -29,9 +32,29 @@ public class Book {
     this.title = title;
   }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(author, title);
+  /**
+   * constructor.
+   *
+   * @param path filename
+   */
+  public Book(final Path path) {
+    this(path.toString());
+  }
+
+  /**
+   * constructor.
+   *
+   * @param name filename
+   */
+  public Book(final String name) {
+    this();
+    Matcher matcher = pattern.matcher(name);
+    if (matcher.matches()) {
+      this.author = matcher.group(2);
+      this.title = matcher.group(3);
+    } else {
+      this.title = name;
+    }
   }
 
   @Override
@@ -49,12 +72,21 @@ public class Book {
     return Objects.equals(author, other.author) && Objects.equals(title, other.title);
   }
 
-  public void setAuthor(String author) {
-    this.author = author;
+  public String getAuthor() {
+    return author;
   }
 
   public String getTitle() {
     return title;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(author, title);
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
   }
 
   public void setTitle(String title) {
