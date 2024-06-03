dd15bab7692ec40d87ebbd6e2dc4b473228d9521
Masanao TAGUCHI <mtag@m-tag.org>
2024-06-03 12:59:25 +0900

    format code.

--- src/main/java/org/m_tag/jfind/books/sqlite/SQLReadingException.java
+++ src/main/java/org/m_tag/jfind/books/sqlite/SQLReadingException.java
@@ -3,16 +3,23 @@ package org.m_tag.jfind.books.sqlite;
 import java.sql.SQLException;
 import org.m_tag.jfind.ReadingException;
 
+/**
+ * Errors on query.
+ */
 public class SQLReadingException extends ReadingException {
 
-  public SQLReadingException(String message, SQLException cause) {
-    super(message, cause);
-    // TODO Auto-generated constructor stub
-  }
-
   /**
-   * 
+   * generated serial.
    */
   private static final long serialVersionUID = 9076932004695335839L;
 
+  /**
+   * Constructor.
+   *
+   * @param message message
+   * @param cause errors on query.
+   */
+  public SQLReadingException(String message, SQLException cause) {
+    super(message, cause);
+  }
 }
