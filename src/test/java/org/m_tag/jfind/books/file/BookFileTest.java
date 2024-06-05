5635aa9d131c7f50298846740326a7eef51d98cf
Masanao TAGUCHI <mtag@m-tag.org>
2024-06-05 17:01:14 +0900

    add test

---
+++ src/test/java/org/m_tag/jfind/books/file/BookFileTest.java
@@ -0,0 +1,25 @@
+package org.m_tag.jfind.books.file;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.m_tag.jfind.books.Book;
+import org.m_tag.jfind.books.Query;
+import org.m_tag.jfind.utils.locate.DbFile;
+
+class BookFileTest {
+
+  @Test
+  void findFromDb() throws IOException {
+    DbFile file = new DbFile("../jFindUtils/src/test/resources/test.db" + File.pathSeparator
+        + "/home/mtag/eclipse-workspace/jFindUtils/" + File.pathSeparator + "../jFindUtils/");
+    Query query = new Query();
+    query.setAuthor("FindFileIterator");
+    Book[] results = BookFile.find(file, query).toArray(Book[]::new);
+    assertEquals(1, results.length);
+    assertTrue(((BookFile) results[0]).getPath().toFile().exists());
+  }
+}
