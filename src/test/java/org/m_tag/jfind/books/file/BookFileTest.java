6ce12fd6cc60e92f09f7fd94a8d2c0210ce3bf11
Masanao TAGUCHI <mtag@m-tag.org>
2024-06-07 12:04:18 +0900

    add test for find

--- src/test/java/org/m_tag/jfind/books/file/BookFileTest.java
+++ src/test/java/org/m_tag/jfind/books/file/BookFileTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 import org.m_tag.jfind.books.Book;
 import org.m_tag.jfind.books.Query;
@@ -19,6 +20,15 @@ class BookFileTest {
     Query query = new Query();
     query.setAuthor("FindFileIterator");
     Book[] results = BookFile.find(file, query).toArray(Book[]::new);
+    assertEquals(1, results.length);
+    assertTrue(((BookFile) results[0]).getPath().toFile().exists());
+  }
+  
+  @Test
+  void findFromDir() throws IOException {
+    Query query = new Query();
+    query.setAuthor("BookFile");
+    Book[] results = BookFile.find(Path.of("src/main/java/"), query).toArray(Book[]::new);
     assertEquals(1, results.length);
     assertTrue(((BookFile) results[0]).getPath().toFile().exists());
   }
