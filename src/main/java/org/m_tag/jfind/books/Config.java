d641e3eb1fadd7d181d0bcfc4e976ce881f11cc8
Masanao TAGUCHI <mtag@m-tag.org>
2024-06-14 01:35:28 +0900

    add Finder class registration

--- src/main/java/org/m_tag/jfind/books/Config.java
+++ src/main/java/org/m_tag/jfind/books/Config.java
@@ -3,10 +3,15 @@ package org.m_tag.jfind.books;
 import jakarta.json.Json;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonReader;
+import jakarta.json.JsonValue;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -18,7 +23,51 @@ import org.m_tag.jfind.books.file.FindLocate;
  */
 public class Config extends Finder {
 
+
+  private static final Map<String, Constructor<? extends Finder>> constructors = new HashMap<>();
+  
+  static {
+    registerFinder("locate", FindLocate.class);
+    registerFinder("folder", FindFolder.class);
+  }
+
+  private Finder getFinder(JsonValue json) {
+    final String type = Finder.readRequiredJsonValue(json, "type").toLowerCase();
+    final String id = Finder.readRequiredJsonValue(json, "id").toLowerCase();
+    final Constructor<? extends Finder> constructor = constructors.get(type);
+    if (constructor == null) {
+      throw new IllegalArgumentException(String.format("unknown finder type:%s", type));
+    }
+    try {
+      return constructor.newInstance(type, id, json);
+    } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+        | InvocationTargetException ex) {
+      throw new IllegalArgumentException(String.format("Illegal constructor at type:%s", type), ex);
+    }
+  }
+
+  protected static void registerFinder(String key, Class<? extends Finder> cl) {
+    try {
+      final Constructor<? extends Finder> constructor =
+          cl.getConstructor(String.class, String.class, JsonValue.class);
+      constructors.put(key, constructor);
+    } catch (NoSuchMethodException ex) {
+      // TODO 直す
+      ex.printStackTrace();
+    }
+  }
+
   private final Map<String, Finder> finds;
+
+  /**
+   * constructor.
+   *
+   * @param file path of config.json
+   * @throws FileNotFoundException configPath file does not exists.
+   */
+  public Config(final File file) throws FileNotFoundException {
+    this(file.toPath());
+  }
 
   /**
    * constructor.
@@ -32,29 +81,28 @@ public class Config extends Finder {
       JsonArray array = reader.readArray();
       Map<String, Finder> finder = new LinkedHashMap<>();
       array.forEach(item -> {
-        final String id = readRequiredJsonValue(item, "id");
-        final String type = readRequiredJsonValue(item, "id").toLowerCase();
-        switch (type) {
-          case "locate":
-            finder.put(id, new FindLocate(item));
-            break;
-          case "folder":
-            finder.put(id, new FindFolder(item));
-            break;
-          default:
-            throw new IllegalArgumentException(String.format("unknown finder type:%s", type));
-        }
+        finder.put(Finder.readRequiredJsonValue(item, "id"), getFinder(item));
       });
       this.finds = finder;
     }
+  }
+
+  /**
+   * constructor.
+   *
+   * @param configPath path of config.json
+   * @throws FileNotFoundException configPath file does not exists.
+   */
+  public Config(final String configPath) throws FileNotFoundException {
+    this(Path.of(configPath));
   }
 
   @Override
   public Stream<Book> find(Query query) throws IOException {
     Stream<Book> ret = null;
     for (Map.Entry<String, Finder> entry : finds.entrySet()) {
-      Finder find = entry.getValue();
-      Stream<Book>  founds = find.find(query);
+      Finder finder = entry.getValue();
+      Stream<Book> founds = finder.find(query);
       if (ret == null) {
         ret = founds;
       } else {
@@ -64,4 +112,28 @@ public class Config extends Finder {
     return ret;
   }
 
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append('[');
+    boolean isFirst = true;
+    for (Map.Entry<String, Finder> entry : finds.entrySet()) {
+      String value = entry.getValue().toString();
+      if (value == null) {
+        return value;
+      }
+      if (isFirst) {
+        isFirst = false;
+      } else {
+        builder.append(',');
+      }
+      builder.append(value);
+    }
+    builder.append(']');
+    return builder.toString();
+  }
+
+  @Override
+  protected void toString(StringBuilder builder) {
+  }
 }
--- src/main/java/org/m_tag/jfind/books/Finder.java
+++ src/main/java/org/m_tag/jfind/books/Finder.java
@@ -1,6 +1,7 @@
 package org.m_tag.jfind.books;
 
 import jakarta.json.JsonValue;
+import java.io.File;
 import java.io.IOException;
 import java.util.stream.Stream;
 
@@ -8,13 +9,33 @@ import java.util.stream.Stream;
  * base class for all finding classes including Config Class to find books with children.
  */
 public abstract class Finder {
+  protected static String readRequiredJsonValue(JsonValue json, final String key) {
+    String id = json.asJsonObject().getString(key);
+    if (id == null) {
+      throw new IllegalArgumentException(String.format("no %s found:%s", key, json.toString()));
+    }
+    return id;
+  }
+  
+  private String id;
+  private String type;
+
   /**
    * constructor.
    */
-  protected Finder() {
-    super();
+  Finder() {
+    this(null, null);
   }
-
+  
+  /**
+   * constructor.
+   */
+  protected Finder(String type, String id) {
+    super();
+    this.type = type;
+    this.id = id;
+  }
+  
   /**
    * find method.
    *
@@ -23,12 +44,29 @@ public abstract class Finder {
    * @throws IOException Error in reading file or db.
    */
   public abstract Stream<Book> find(final Query query) throws IOException;
+  
+    
+  protected abstract void toString(StringBuilder builder);
+  
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append('{');
+    builder.append("\"id\":\"");
+    builder.append(id);
+    builder.append("\",\"type\":\"");
+    builder.append(type);
+    builder.append('\"');
+    toString(builder);
+    builder.append('}');
+    return builder.toString();
+  }
 
-  protected static String readRequiredJsonValue(JsonValue json, final String key) {
-    String id = json.asJsonObject().getString(key);
-    if (id == null) {
-      throw new IllegalArgumentException(String.format("no %s found:%s", key, json.toString()));
+  protected void escape(StringBuilder builder, final String pathString) {
+    if (File.separatorChar == '\\') {
+      builder.append(pathString.replace('\\', '/'));
+    } else {
+      builder.append(pathString);
     }
-    return id;
   }
 }
