a074d3e5247f228bc340c1641524cb9150bcf52d
Masanao TAGUCHI <mtag@m-tag.org>
2024-06-15 20:12:08 +0900

    implements matching and adds unit test for query

--- src/main/java/org/m_tag/jfind/books/Query.java
+++ src/main/java/org/m_tag/jfind/books/Query.java
@@ -1,6 +1,12 @@
 package org.m_tag.jfind.books;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * query for finding books.
@@ -11,7 +17,15 @@ public class Query {
   private boolean exists = true;
   private String replaceFrom;
   private String replaceTo;
-  
+  private final List<Pattern> patterns = new ArrayList<>();
+  private String keyword;
+  private boolean caseSensitive;
+
+  public Query() {
+    super();
+    updatePattern();
+  }
+
   public void setReplacement(String from, String to) {
     this.replaceFrom = from;
     this.replaceTo = to;
@@ -31,6 +45,7 @@ public class Query {
 
   public void setExists(boolean exists) {
     this.exists = exists;
+    updatePattern();
   }
 
   public String getAuthor() {
@@ -39,6 +54,7 @@ public class Query {
 
   public void setAuthor(String author) {
     this.author = author;
+    updatePattern();
   }
 
   public String getTitle() {
@@ -48,7 +64,25 @@ public class Query {
   public void setTitle(String title) {
     this.title = title;
   }
-  
+
+  public String getKeyword() {
+    return keyword;
+  }
+
+  public void setKeyword(String keyword) {
+    this.keyword = keyword;
+    updatePattern();
+  }
+
+  public boolean isCaseSensitive() {
+    return caseSensitive;
+  }
+
+  public void setCaseSensitive(boolean caseSensitive) {
+    this.caseSensitive = caseSensitive;
+    updatePattern();
+  }
+
   /**
    * matching found path and query.
    *
@@ -56,8 +90,76 @@ public class Query {
    * @return matched or not
    */
   public boolean matches(final Path path) {
+    if (patterns.isEmpty()) {
+      return true;
+    }
     String name = path.getFileName().toString();
-    return (author != null && name.contains(author))
-        || (title != null && name.contains(title));
+    for (Pattern pattern : patterns) {
+      Matcher matcher = pattern.matcher(name);
+      if (!matcher.matches()) {
+        // and search
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private String escape(String word) {
+    return Pattern.quote(word);
+  }
+
+  private void updatePattern() {
+    synchronized (patterns) {
+      patterns.clear();
+
+      if (keyword != null) {
+        patterns.add(Pattern.compile(".*(" + escape(keyword) + ").*", 0));
+      }
+      if (title != null) {
+        patterns.add(Pattern.compile(".*.*\\].*(" + escape(title) + ").*"));
+      }
+      if (author != null) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(".*\\[[^\\[\\]]*(");
+        builder.append(escape(author));
+        builder.append(")[^\\[\\]]*\\].*");
+        patterns.add(Pattern.compile(builder.toString()));
+      }
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    String file = null;
+    final Query query = new Query();
+    query.setExists(false);
+    for (int i = 0; i < args.length; i++) {
+      String arg = args[i];
+      switch (arg) {
+        case "-f":
+          file = args[++i];
+          break;
+        case "-a":
+          query.setAuthor(args[++i]);
+          break;
+        case "-t":
+          query.setTitle(args[++i]);
+          break;
+        case "-k":
+          query.setKeyword(args[++i]);
+          break;
+        default:
+          query.setKeyword(arg);
+          break;
+      }
+    }
+    if (file == null) {
+      System.err.println(
+          String.format("Usage: java %s -f json [-a author] [-t title] [-k keywork] [keyword]",
+              Query.class.getName()));
+      System.exit(-1);
+    }
+    for (Book item : new Config(Path.of(file)).find(query).toList()) {
+      System.out.println(item);
+    }
   }
 }
---
+++ src/test/java/org/m_tag/jfind/books/QueryTest.java
@@ -0,0 +1,22 @@
+package org.m_tag.jfind.books;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+
+class QueryTest {
+
+  @Test
+  void testByAuthor() {
+    Query query = new Query();
+    query.setAuthor("test");
+    assertTrue(query.matches(Path.of("(sample) [a×test] test")));
+    assertTrue(!query.matches(Path.of("(sample) [a×tes] test")));
+    assertTrue(!query.matches(Path.of("(sample) [a] test")));
+    assertTrue(!query.matches(Path.of("(test) [a] sample")));
+    assertTrue(!query.matches(Path.of("[a] sample")));
+    assertTrue(query.matches(Path.of("[test] sample")));
+    assertTrue(query.matches(Path.of("(sample) [a×test×b] xyz")));
+  }
+
+}
