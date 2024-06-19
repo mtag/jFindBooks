e9215ac1ed519ca615de496924ff2eb499f1af83
Masanao Taguchi <mtag@jp.ibm.com>
2024-06-19 13:51:59 +0900

    remove unused import

--- src/main/java/org/m_tag/jfind/books/sqlite/CalibreFinder.java
+++ src/main/java/org/m_tag/jfind/books/sqlite/CalibreFinder.java
@@ -1,7 +1,6 @@
 package org.m_tag.jfind.books.sqlite;
 
 import jakarta.json.JsonObject;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import org.m_tag.jfind.books.Finder;
